0dbc2749184ff5ca99ba10d8a18640a066a77dc2
nicklasbns <nicklasbns@gmail.com>
2024-08-28 15:58:32 +0200

    added read and write functions
    Co-authored-by: Caniuss <johannes@orager.dk>
    Co-authored-by: Joakim <61437971+0xjogge@users.noreply.github.com>
    Co-authored-by: Kevin <kevingravesen@gmail.com>
    Co-authored-by: Elias <elia07891@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,46 +1,73 @@
 ﻿using System.Collections;
+using System.Net;
 using System.Text.RegularExpressions;
 
+if (args.Length == 0) return;
 
-try
+if (args[0] == "read")
 {
-
-    List<Cheep> cheeps = [];
-
-    using StreamReader reader = new("./chirp_cli_db.csv");
-    string? line;
-
-    reader.ReadLine();
-
-    while ((line = reader.ReadLine()) != null)
+    try
     {
-        Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+        List<Cheep> cheeps = [];
 
-        //Separating columns to array
-        string[] X = CSVParser.Split(line);
+        using StreamReader reader = new("./chirp_cli_db.csv");
+        string? line;
 
-        var offset = DateTimeOffset.FromUnixTimeSeconds(long.Parse(X[2]));
-        var time = offset.LocalDateTime;
-        cheeps.Add(new Cheep {Author = X[0], Message = X[1], Timestamp = time});
+        reader.ReadLine();
 
+        while ((line = reader.ReadLine()) != null)
+        {
+            Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+
+            //Separating columns to array
+            string[] X = CSVParser.Split(line);
+
+            var offset = DateTimeOffset.FromUnixTimeSeconds(long.Parse(X[2]));
+            var time = offset.LocalDateTime;
+            cheeps.Add(new Cheep {Author = X[0], Message = X[1], Timestamp = time});
+
+        }
+
+        foreach (var cheep in cheeps)
+        {
+            // Console.WriteLine(cheep.Author + " " + cheep.Message + " " + cheep.Timestamp);
+            Console.WriteLine($"this person \"{cheep.Author}\" sent {cheep.Message} at: {cheep.Timestamp}");
+        }
     }
-
-    foreach (var cheep in cheeps)
+    catch (IOException e)
     {
-        // Console.WriteLine(cheep.Author + " " + cheep.Message + " " + cheep.Timestamp);
-        Console.WriteLine($"this person \"{cheep.Author}\" sent {cheep.Message} at: {cheep.Timestamp}");
+        Console.WriteLine("The file could not be read:");
+        Console.WriteLine(e.Message);
     }
-
 }
-catch (IOException e)
+else if (args[0] == "cheep")
 {
-    Console.WriteLine("The file could not be read:");
-    Console.WriteLine(e.Message);
+    if (args.Length < 2)
+    {
+        Console.WriteLine("You have not written a cheep");
+        return;
+    }
+
+    try
+    {
+        StreamWriter file = new("./chirp_cli_db.csv", append: true);
+        file.WriteLine($"{System.Environment.MachineName},\"{args[1].Trim()}\",{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}");
+        file.Close();
+        Console.WriteLine("Cheep cheeped!");
+    }
+    catch (IOException e) {
+        Console.WriteLine("The file could not be read:");
+        Console.WriteLine(e.Message);
+    }
+}
+else
+{
+    Console.WriteLine("No recognized command, use \"cheep\" or \"read\"");
 }
 
 class Cheep
 {
-    public string Author;
-    public string Message;
-    public DateTime Timestamp;
+    public required string Author;
+    public required string Message;
+    public required DateTime Timestamp;
 }
