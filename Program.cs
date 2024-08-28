d72d5f6185d42a2d883dc62958e35e1ac43acb28
Caniuss <johannes@orager.dk>
2024-08-28 14:49:06 +0200

    start on program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+
+
+try
+{
+
+    Dictionary<string, ArrayList> map = new Dictionary<string, ArrayList>();
+
+    using (StreamReader reader = new StreamReader("chirp_cli_db.csv"))
+    {
+        string line; 
+
+        string headers = reader.ReadLine();
+        
+        foreach (var item in headers.Split(','))
+        {
+            map.Add(item, new ArrayList());
+        }
+
+        while ((line = reader.ReadLine()) != null)
+        {            
+            Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+
+            //Separating columns to array
+            string[] X = CSVParser.Split(line);
+            
+
+        }
+    }
+
+}
+catch (IOException e)
+{
+    Console.WriteLine("The file could not be read:");
+    Console.WriteLine(e.Message);
+}
