63b3ba76e95d5340acb27a401aed9412c3ba00e2
joaan <61437971+0xjogge@users.noreply.github.com>
2024-09-04 18:50:51 +0200

    Check if database is already created
    CSVDatabse now checks if the CSV database file is created. If it is not, it will create it.

--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -6,9 +6,25 @@ using CsvHelper.Configuration;
 
 public class CSVDatabase<T> : IDatabaseRepository<T>
 {
+    private readonly string databasePath = "../chirp_cli_db.csv";
+
+    private void CreateDatabaseFileIfMissing()
+    {
+        if (File.Exists(databasePath)) return;
+
+        using var stream = File.Create(databasePath);
+        using var writer = new StreamWriter(stream);
+        using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+        csv.WriteHeader<T>();
+        csv.NextRecord(); // Create a newline, such that new records wont be on headerline.
+    }
+
     public IEnumerable<T> Read(int? limit = null)
     {
-        using var reader = new StreamReader("../chirp_cli_db.csv");
+        CreateDatabaseFileIfMissing();
+
+        using var reader = new StreamReader(databasePath);
         using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
         var records = csv.GetRecords<T>();
         return records.ToList();
@@ -21,7 +37,10 @@ public class CSVDatabase<T> : IDatabaseRepository<T>
         {
             HasHeaderRecord = false,
         };
-        using var stream = File.Open("../chirp_cli_db.csv", FileMode.Append);
+
+        CreateDatabaseFileIfMissing();
+
+        using var stream = File.Open(databasePath, FileMode.Append);
         using var writer = new StreamWriter(stream);
         using var csv = new CsvWriter(writer, config);
 
