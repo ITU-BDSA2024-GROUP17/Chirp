08da8ab5e69f8df1e4b423fee1fc9d6fb7edcf61
joaan <61437971+0xjogge@users.noreply.github.com>
2024-09-30 07:26:35 +0200

    Add demo messages

--- SimpleDB/data/schema.sql
+++ SimpleDB/data/schema.sql
@@ -15,7 +15,15 @@ create table message (
 );
 
 -- add test data
-insert into user (username, email) values ("aabb", "ab@ab.com");
--- insert into message (author_id, text, pub_date) values (1, "hello", 1234567890);
--- insert into message (author_id, text, pub_date) values (1, "Test message 2", 1234888890);
--- insert into message (author_id, text, pub_date) values (1, "Test message but 3", 1234888891);
+insert into user (username, email) values ("ropf", "ropf@itu.com");
+insert into user (username, email) values ("kegr", "kegr@itu.com");
+insert into user (username, email) values ("jgjo", "jgjo@itu.com");
+insert into message (author_id, text, pub_date) values (1, "Test message 1", 1234567890);
+insert into message (author_id, text, pub_date) values (1, "Test message 2", 1234567890);
+insert into message (author_id, text, pub_date) values (1, "Test message 3", 1234567890);
+insert into message (author_id, text, pub_date) values (2, "Test message 1", 1234888890);
+insert into message (author_id, text, pub_date) values (2, "Test message 2", 1234888890);
+insert into message (author_id, text, pub_date) values (2, "Test message 3", 1234888890);
+insert into message (author_id, text, pub_date) values (3, "Test message 1", 1234888891);
+insert into message (author_id, text, pub_date) values (3, "Test message 2", 1234888891);
+insert into message (author_id, text, pub_date) values (3, "Test message 3", 1234888891);
