926a1c1f915e19a901c2beb7c50e73b797d028c9
joaan <61437971+0xjogge@users.noreply.github.com>
2024-09-30 09:46:26 +0200

    Update schema file to have every user field

--- SimpleDB/data/schema.sql
+++ SimpleDB/data/schema.sql
@@ -3,7 +3,8 @@ drop table if exists user;
 create table user (
   user_id integer primary key autoincrement,
   username string not null,
-  email string not null
+  email string not null,
+  pw_hash string not null
 );
 
 drop table if exists message;
@@ -13,17 +14,3 @@ create table message (
   text string not null,
   pub_date integer
 );
-
--- add test data
-insert into user (username, email) values ("ropf", "ropf@itu.com");
-insert into user (username, email) values ("kegr", "kegr@itu.com");
-insert into user (username, email) values ("jgjo", "jgjo@itu.com");
-insert into message (author_id, text, pub_date) values (1, "Test message 1", 1234567890);
-insert into message (author_id, text, pub_date) values (1, "Test message 2", 1234567890);
-insert into message (author_id, text, pub_date) values (1, "Test message 3", 1234567890);
-insert into message (author_id, text, pub_date) values (2, "Test message 1", 1234888890);
-insert into message (author_id, text, pub_date) values (2, "Test message 2", 1234888890);
-insert into message (author_id, text, pub_date) values (2, "Test message 3", 1234888890);
-insert into message (author_id, text, pub_date) values (3, "Test message 1", 1234888891);
-insert into message (author_id, text, pub_date) values (3, "Test message 2", 1234888891);
-insert into message (author_id, text, pub_date) values (3, "Test message 3", 1234888891);
