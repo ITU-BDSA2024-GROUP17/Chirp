d0580fe6e01b7c935fa0d753aa73dc38ba41a68a
johannes67890 <johannes@orager.dk>
2024-10-19 17:47:44 +0200

    A few doc comments

--- src/Web/Utilities/Util.cs
+++ src/Web/Utilities/Util.cs
@@ -6,6 +6,11 @@ public static class TimeUtilties
     private static readonly long unixDay = 86400;
     private static readonly long unixYear = 31536000;
 
+    /// <summary>
+    /// Truns a DateTime object into a formatted string.
+    /// </summary>
+    /// <param name="timestamp">The DateTime timstamp to format</param>
+    /// <returns>A pretty formated string</returns>
     public static string FormatDateTimePretty(DateTime timestamp)
     {
         long unixTimeStamp = ((DateTimeOffset)timestamp).ToUnixTimeSeconds();
@@ -29,6 +34,11 @@ public static class TimeUtilties
         }
     }
 
+    /// <summary>
+    /// Turns a Unix timestamp into a formatted string.
+    /// </summary>
+    /// <param name="timestamp">The Unix timestamp to format</param>
+    /// <returns>A pretty formatted string</returns>
     public static string FormatUnixTimeRaw(long timestamp)
     {
         return DateTimeOffset.FromUnixTimeSeconds(timestamp).ToString("yyyy-MM-dd HH:mm:ss");
