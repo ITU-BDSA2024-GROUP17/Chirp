771b9f3e0d3c66e5e8cdcb0560cb00012e7e02f9
joaan <61437971+0xjogge@users.noreply.github.com>
2024-10-21 09:35:49 +0200

    Impletment basic author tests

---
+++ test/Chirp.Infrastructure.Tests/Repositories/AuthorRepositoryTests.cs
@@ -0,0 +1,41 @@
+using Chirp.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chirp.Infrastructure.Tests.Repositories;
+
+public class AuthorRepositoryTests
+{
+    private readonly CheepDbContext _cheepDbContext;
+    private readonly AuthorRepository _authorRepository;
+
+    public AuthorRepositoryTests()
+    {
+        var options = new DbContextOptionsBuilder<CheepDbContext>()
+            .UseInMemoryDatabase(databaseName: "AuthorRepositoryTests")
+            .Options;
+
+        _cheepDbContext = new CheepDbContext(options);
+        DbInitializer.SeedDatabase(_cheepDbContext);
+
+        _authorRepository = new AuthorRepository(_cheepDbContext);
+    }
+
+    [Fact]
+    public void GetAllAuthors()
+    {
+        var authors = _authorRepository.GetAuthors();
+
+        Assert.Equal(4, authors.Count);
+    }
+
+    [Theory]
+    [InlineData("John", 1, 2)]
+    [InlineData("o", 1, 3)]
+    [InlineData("smith", 1, 2)]
+    public async Task SearchCheeps(string search, int page, int expected)
+    {
+        var authors = (await _authorRepository.SearchAuthors(search, page)).ToList();
+
+        Assert.Equal(expected, authors.Count);
+    }
+}
