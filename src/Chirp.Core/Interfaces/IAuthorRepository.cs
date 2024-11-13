bfe898eb0f027cb68a58b9936be0fd2599514d90
joaan <61437971+0xjogge@users.noreply.github.com>
2024-11-13 19:00:38 +0100

    Add new likedcheeps getters

--- src/Chirp.Core/Interfaces/IAuthorRepository.cs
+++ src/Chirp.Core/Interfaces/IAuthorRepository.cs
@@ -10,6 +10,8 @@ public interface IAuthorRepository
     public Task<List<Author>> GetAuthors(int page);
     public Task<Author> GetAuthorByField(string author, Func<Author, string> field);
     public Task<List<Cheep>> GetCheeps(string author, int page);
-    public Task<int> CountCheeps(string author);
+    public Task<int> GetCheepsCount(string author);
+    public Task<List<Cheep>> GetLiked(string author, int page);
+    public Task<int> GetLikedCount(string author);
     public Task<List<Author>> SearchAuthors(string searchQuery, int page);
 }
