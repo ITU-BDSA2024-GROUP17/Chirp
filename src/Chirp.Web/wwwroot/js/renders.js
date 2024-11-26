3245b11220cd1257883dbf10d81e33979d1b7312
Kevin <kegr@itu.dk>
2024-11-26 18:18:27 +0100

    Added a way to view the previous revisions of a cheep

--- src/Chirp.Web/wwwroot/js/renders.js
+++ src/Chirp.Web/wwwroot/js/renders.js
@@ -4,15 +4,33 @@ function ToggleEditCheep(cheepId){
     let messageElem = document.getElementById("cheepmessage-" + cheepId);
     let editElem = document.getElementById("cheepedit-" + cheepId);
 
-    if (messageElem.style.display == "none"){
-        messageElem.style.display = "";
-        editElem.style.display = "none";
+    let revisionList = document.getElementById("cheeprevision-" + cheepId);
+    revisionList.style.display = "none";
 
-    }else{
+    //If the edit input box isn't shown. Enable it and disable the original cheepMessage box
+    if (messageElem.style.display == ""){
         messageElem.style.display = "none";
         editElem.style.display = "";
-        editElem.value = messageElem.value;
         document.getElementById("cheepeditbox-" + cheepId).focus();
+    }else{
+        messageElem.style.display = "";
+        editElem.style.display = "none";
+    }
+}
+
+function ToggleRevisions(cheepId){
+    if (document.getElementById("cheepedit-" + cheepId).style.display == "") { return; }
+
+    console.log("Toggle Edit!");
+    let revisionList = document.getElementById("cheeprevision-" + cheepId);
+    let messageElem = document.getElementById("cheepmessage-" + cheepId);
+
+    if (revisionList.style.display == "none"){
+        messageElem.style.display = "none";
+        revisionList.style.display = "";
+    }else{
+        revisionList.style.display = "none";
+        messageElem.style.display = "";
     }
 }
 
