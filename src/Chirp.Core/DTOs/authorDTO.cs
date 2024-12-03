ab10abae1c60813391b4b8b4e0d178ebaf16d15b
joaan <61437971+0xjogge@users.noreply.github.com>
2024-12-03 08:31:23 +0100

    Add punctuation

--- src/Chirp.Core/DTOs/authorDTO.cs
+++ src/Chirp.Core/DTOs/authorDTO.cs
@@ -3,15 +3,15 @@ namespace Chirp.Core.DTOs;
 public class CreateAuthorDto
 {
     /// <summary>
-    /// Id of the author
+    /// Id of the author.
     /// </summary>
     public required string Id { get; set; }
     /// <summary>
-    /// Username of the author
+    /// Username of the author.
     /// </summary>
     public required string UserName { get; set; }
     /// <summary>
-    /// A URL to the author's avatar image
+    /// URL to the author's avatar image.
     /// </summary>
     public string? Avatar { get; set; }
 }
--- src/Chirp.Core/DTOs/cheepDTO.cs
+++ src/Chirp.Core/DTOs/cheepDTO.cs
@@ -3,11 +3,11 @@ namespace Chirp.Core.DTOs;
 public class CreateCheepDto
 {
     /// <summary>
-    /// Author of the cheep
+    /// Author of the cheep.
     /// </summary>
     public required string Author { get; set; }
     /// <summary>
-    /// Initial message in the cheep
+    /// Initial message in the cheep.
     /// </summary>
     public required string Message { get; set; }
 }
