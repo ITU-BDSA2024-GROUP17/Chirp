db77c89658b0fbd37f9883e153f9f0b94674ab63
joaan <61437971+0xjogge@users.noreply.github.com>
2024-12-03 08:31:35 +0100

    Wrote entity comments

--- src/Chirp.Core/Entities/CheepLike.cs
+++ src/Chirp.Core/Entities/CheepLike.cs
@@ -2,5 +2,8 @@ namespace Chirp.Core.Entities;
 
 public class CheepLike
 {
+    /// <summary>
+    /// Timestamp of when the author liked the cheep.
+    /// </summary>
     public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
 }
--- src/Chirp.Core/Entities/CheepRevision.cs
+++ src/Chirp.Core/Entities/CheepRevision.cs
@@ -4,9 +4,20 @@ namespace Chirp.Core.Entities;
 
 public class CheepRevision
 {
+    /// <summary>
+    /// Id to the revision.
+    /// </summary>
     [Key]
     public Guid Id { get; set; }
+    /// <summary>
+    /// The message the that this revision contains.
+    /// The message is limited to at most 160 characters.
+    /// </summary>
     [StringLength(160)]
     public required string Message { get; set; }
+    /// <summary>
+    /// Timestamp of when this revision was made.
+    /// This can be used to determine when an edit to a cheep is made.
+    /// </summary>
     public required DateTime TimeStamp { get; set; }
 }
