0f121479e731b4553a591b252dde98adf01b9de5
joaan <61437971+0xjogge@users.noreply.github.com>
2024-12-03 07:59:04 +0100

    Add DTO comments

--- src/Chirp.Core/DTOs/authorDTO.cs
+++ src/Chirp.Core/DTOs/authorDTO.cs
@@ -2,7 +2,16 @@ namespace Chirp.Core.DTOs;
 
 public class CreateAuthorDto
 {
+    /// <summary>
+    /// Id of the author
+    /// </summary>
     public required string Id { get; set; }
+    /// <summary>
+    /// Username of the author
+    /// </summary>
     public required string UserName { get; set; }
+    /// <summary>
+    /// A URL to the author's avatar image
+    /// </summary>
     public string? Avatar { get; set; }
 }
--- src/Chirp.Core/DTOs/cheepDTO.cs
+++ src/Chirp.Core/DTOs/cheepDTO.cs
@@ -2,6 +2,12 @@ namespace Chirp.Core.DTOs;
 
 public class CreateCheepDto
 {
+    /// <summary>
+    /// Author of the cheep
+    /// </summary>
     public required string Author { get; set; }
+    /// <summary>
+    /// Initial message in the cheep
+    /// </summary>
     public required string Message { get; set; }
 }
