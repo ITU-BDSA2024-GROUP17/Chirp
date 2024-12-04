a0fe1802620928754a4d4181d0137ce5b047587a
johannes67890 <johannes@orager.dk>
2024-12-04 12:09:20 +0100

    Remove commented-out code for posting comments on Cheeps

--- src/Chirp.Web/Pages/User/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/User/UserTimeline.cshtml.cs
@@ -91,49 +91,6 @@ public class UserModel(AuthorService authorService, CheepService cheepService) :
         return LocalRedirect(Request.Path.ToString());
     }
 
-    public async Task<IActionResult> OnPostCommentCheepAsync(int CommentCheep, string CommentText)
-    {
-        if (string.IsNullOrWhiteSpace(CommentText))
-        {
-            ModelState.AddModelError("", "Comment cannot be empty.");
-            return Page();
-        }
-
-        var UserId = (User.FindFirst(ClaimTypes.NameIdentifier)?.Value) ?? throw new Exception("User not found!");
-        var author = await _authorService.GetAuthor(UserId) ?? throw new Exception("User not found!");
-
-
-        try
-        {
-
-            Cheep cheep = new()
-            {
-                AuthorId = UserId,
-                Author = author,
-                Revisions = new List<CheepRevision>
-                {
-                    new CheepRevision
-                    {
-                        Message = CommentText,
-                        TimeStamp = DateTime.UtcNow
-                    }
-                },
-                Likes = []
-            };
-
-            await _cheepService.PostComment(CommentCheep, cheep);
-
-            // Reload page
-            return LocalRedirect(Request.Path.ToString());
-        }
-        catch (InvalidDataException)
-        {
-            return LocalRedirect(Request.Path.ToString());
-        }
-
-    }
-
-
     public async Task<IActionResult> OnPostCheepAsync(string returnUrl)
     {
         if (User.Identity == null || !User.Identity.IsAuthenticated) throw new UnauthorizedAccessException("User is not logged in!");
