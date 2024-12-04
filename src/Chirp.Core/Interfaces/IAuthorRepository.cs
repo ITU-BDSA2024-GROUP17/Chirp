77ac09b60aad1899ff7a0242a0329206d5fae8ec
joaan <61437971+0xjogge@users.noreply.github.com>
2024-12-04 17:35:51 +0100

    Added cheep repo docs

--- src/Chirp.Core/Interfaces/IAuthorRepository.cs
+++ src/Chirp.Core/Interfaces/IAuthorRepository.cs
@@ -10,18 +10,21 @@ public interface IAuthorRepository
     /// <param name="author">Author object to be inserted into the database.</param>
     /// <returns>Task containing the Author that have been created.</returns>
     public Task<Author> CreateAuthor(Author author);
+
     /// <summary>
     /// Fetch an author from the database based on the author's id.
     /// </summary>
     /// <param name="id">The id of the author that should be retreived.</param>
     /// <returns>Task containing the Author, if a author with the id was found.</returns>
     public Task<Author?> GetAuthor(string id);
+
     /// <summary>
     /// Fetch an author from the database based on the author's username.
     /// </summary>
     /// <param name="name">Username of the author that should be retreived.</param>
     /// <returns>Task containing the Author, if a author with the username was found.</returns>
     public Task<Author?> GetAuthorByName(string name);
+
     /// <summary>
     /// Fetch a list of authors paginated by a page number.
     /// Each page includes 32 entries, unless less is found.
@@ -29,6 +32,7 @@ public interface IAuthorRepository
     /// <param name="page">Page to be retrieved.</param>
     /// <returns>Task containing a list of authors found on the requested page.</returns>
     public Task<List<Author>> GetAuthors(int page);
+
     /// <summary>
     /// Retrieves an Author based on a specific field.
     ///
@@ -42,6 +46,7 @@ public interface IAuthorRepository
     /// <param name="field">A function to select the field to search by.</param>
     /// <returns>Task containing a Author found with the inputed field.</returns>
     public Task<Author> GetAuthorByField(string author, Func<Author, string> field);
+
     /// <summary>
     /// Retrieves a paginated list of Cheeps from a specific Author
     /// Each page includes 32 entries, unless less is found.
@@ -50,12 +55,14 @@ public interface IAuthorRepository
     /// <param name="page">The page to retrieve.</param>
     /// <returns>Task containing a list of cheeps.</returns>
     public Task<List<Cheep>> GetCheeps(string author, int page);
+
     /// <summary>
     /// Retrieves the total number of cheeps that the author have sent.
     /// </summary>
     /// <param name="author">Id of the author to count the cheeps of.</param>
     /// <returns>Task containing the count of cheeps.</returns>
     public Task<int> GetCheepsCount(string author);
+
     /// <summary>
     /// Retrieves a list of cheeps belonging to the author.
     /// Along with the cheeps from authors that is followed by the author.
@@ -72,6 +79,7 @@ public interface IAuthorRepository
     /// <param name="author">Id of the author to fetch the count of cheeps from.</param>
     /// <returns>Task containing the total count cheeps on an authors timeline.</returns>
     public Task<int> GetCheepsTimelineCount(string author);
+
     /// <summary>
     /// Retrieves a list of cheeps that the authors have liked.
     /// Each page includes 32 entries, unless less is found.
@@ -80,12 +88,14 @@ public interface IAuthorRepository
     /// <param name="page">Page to be retrieved.</param>
     /// <returns>Task containing the list of cheeps liked by the author.</returns>
     public Task<List<Cheep>> GetLiked(string author, int page);
+
     /// <summary>
     /// Retrieves the total count of cheeps that an author have liked.
     /// </summary>
     /// <param name="author">Id of the author to count the likes from.</param>
     /// <returns>Task containing the total count of liked cheeps.</returns>
     public Task<int> GetLikedCount(string author);
+
     /// <summary>
     /// Retrieves a list of authors based on a search term.
     /// Each page includes 32 entries, unless less is found.
@@ -94,18 +104,21 @@ public interface IAuthorRepository
     /// <param name="page">Page to be retrieved.</param>
     /// <returns>Task containing the authors found based on the search term.</returns>
     public Task<List<Author>> SearchAuthors(string searchQuery, int page);
+
     /// <summary>
     /// Retrieves a list of authors that is being followed by the provided author.
     /// </summary>
     /// <param name="authorId">Id of the author to fetch the following from.</param>
     /// <returns>Task containing a list of authors.</returns>
     public Task<List<Author>> GetFollowing(string authorId);
+
     /// <summary>
     /// Retrieves a list of authors that is following the provided author.
     /// </summary>
     /// <param name="authorId">Id of the author to fetch the followers from.</param>
     /// <returns>Task containing a list of authors.</returns>
     public Task<List<Author>> GetFollowers(string authorId);
+
     /// <summary>
     /// Add a follow to an author.
     /// </summary>
@@ -113,6 +126,7 @@ public interface IAuthorRepository
     /// <param name="followeeId">Id of the author that is starting to being followed.</param>
     /// <returns>Task containing the new author object of the author that is following.</returns>
     public Task<Author> Follow(string followerId, string followeeId);
+
     /// <summary>
     /// Remove a follow to an author.
     /// </summary>
@@ -120,6 +134,7 @@ public interface IAuthorRepository
     /// <param name="followeeId">Id of the author that is stopping to being followed.</param>
     /// <returns>Task containing the new author object of the author that is no longer following.</returns>
     public Task<Author> Unfollow(string followerId, string followeeId);
+
     /// <summary>
     /// Update the phone number of an author.
     /// </summary>
@@ -127,6 +142,7 @@ public interface IAuthorRepository
     /// <param name="phoneNumber">New phone number to be updated on the author.</param>
     /// <returns>Task containing the updated author.</returns>
     public Task<Author> UpdateAuthorPhoneNumber(string id, string phoneNumber);
+
     /// <summary>
     /// Update the avatar of an author.
     /// </summary>
--- src/Chirp.Core/Interfaces/ICheepRepository.cs
+++ src/Chirp.Core/Interfaces/ICheepRepository.cs
@@ -10,23 +10,27 @@ public interface ICheepRepository
     /// <param name="cheep">Cheep to save in the database.</param>
     /// <returns>Task containing to cheep that was added to the database.</returns>
     public Task<Cheep> CreateCheep(Cheep cheep);
+
     /// <summary>
     /// Retrieves an cheep based on its id.
     /// </summary>
     /// <param name="id">Id of the cheep to retrieve.</param>
     /// <returns>Task containing the cheep, if it was found based on its id.</returns>
     public Task<Cheep?> GetCheep(int id);
+
     /// <summary>
     /// Retrieve the cheeps on a specific page.
     /// </summary>
     /// <param name="page">Page to be retrieved.</param>
     /// <returns>Task containing a list of cheeps on the requested page.</returns>
     public Task<List<Cheep>> GetCheeps(int page);
+
     /// <summary>
     /// Retrieve the total count of cheeps.
     /// </summary>
     /// <returns>Task containing the total count of cheeps.</returns>
     public Task<int> CountCheeps();
+
     /// <summary>
     /// Update the message on a cheep.
     /// This will create a new revision with the new information.
@@ -35,9 +39,43 @@ public interface ICheepRepository
     /// <param name="cheepRevision">The new revision to be added to the cheep.</param>
     /// <returns>Task containing the updated cheep.</returns>
     public Task<Cheep> UpdateCheep(int cheepId, CheepRevision cheepRevision);
+
+    /// <summary>
+    /// Retrieve a cheeps based on a given search term.
+    /// </summary>
+    /// <param name="searchQuery">The string to search for in the cheeps.</param>
+    /// <param name="page">The page to retrieve.</param>
+    /// <returns>Task containing a list of cheeps.</returns>
     public Task<List<Cheep>> SearchCheeps(string searchQuery, int page);
+
+    /// <summary>
+    /// Post a comment to an already existing cheep.
+    /// </summary>
+    /// <param name="CheepToCommentId">Id of the cheep to comment on.</param>
+    /// <param name="comment">A cheep to post on an cheep.</param>
+    /// <returns>Task</returns>
     public Task PostComment(int CheepToCommentId, Cheep comment);
+
+    /// <summary>
+    /// Apply a like on a cheep based from the given author.
+    /// </summary>
+    /// <param name="cheepId">Id of the cheep to like.</param>
+    /// <param name="authorId">Id of the author that liked.</param>
+    /// <returns>Task containing the updated cheep.</returns>
     public Task<Cheep> LikeCheep(int cheepId, string authorId);
+
+    /// <summary>
+    /// Remove the like from a cheep that was previously given by an author.
+    /// </summary>
+    /// <param name="cheepId">Id of the cheep to remove the like from.</param>
+    /// <param name="authorId">Id of the author that removed the like.</param>
+    /// <returns>Task containing the updated cheep.</returns>
     public Task<Cheep> UnlikeCheep(int cheepId, string authorId);
+
+    /// <summary>
+    /// Removes a given cheep from the database.
+    /// </summary>
+    /// <param name="cheepId">Id of the cheep to remove.</param>
+    /// <returns>Task</returns>
     public Task DeleteCheep(int cheepId);
 }
