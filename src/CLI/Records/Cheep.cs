7e8ae919d36a92f0f0650a14f3a1b7f76dcfc729
joaan <61437971+0xjogge@users.noreply.github.com>
2024-12-28 14:19:31 +0100

    Wrote XML comment for the Cheep record

--- src/CLI/Records/Cheep.cs
+++ src/CLI/Records/Cheep.cs
@@ -1,3 +1,10 @@
 namespace CLI.Records;
 
+/// <summary>
+/// A record representing a message.
+/// The messages are made by users and saved in the database.
+/// </summary>
+/// <param name="Author">Name of the author that wrote the message.</param>
+/// <param name="Message">Text in the message.</param>
+/// <param name="Timestamp">Unix time of when the message was made.</param>
 public record Cheep(string Author, string Message, long Timestamp);
